5e9ae72f4576d771a5f5f1855790b78232c3e3cb
Deeachain <Deeachain@163.com>
2020-08-12 23:05:28 +0800

    20200812

--- 剑指/二叉树的镜像.py
+++ 剑指/二叉树的镜像.py
@@ -11,8 +11,8 @@ class Solution:
     # 返回镜像树的根节点
     def Mirror(self, root):
         # write code here
-        if root != None:
-            root.left,root.right = root.right,root.left
-            self.Mirror(root.left)
-            self.Mirror(root.right)
+        if not root:
+            return None
+        root.left, root.right = self.Mirror(root.right), self.Mirror(root.left)
+        return root
 
